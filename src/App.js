bd9f38e95d4a91d8ebd3b0c35d239eaf91406507
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-01 22:00:33 +0000

    created js for each page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home'; // Import your Home component here
+import Projects from './Projects'; // Import your Projects component here
+import Contact from './Contact'; // Import your Contact component here
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+          {/* Define other routes here */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
