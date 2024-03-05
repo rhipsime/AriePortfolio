7939841c5a5cf48d0408aa4b87d275cfc6756153
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-05 19:28:20 +0000

    changed styling of home page

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ const Home = () => {
     textAlign: 'center',
     padding: '20px',
     backgroundColor: '#ffd8e7', // Background colour from my palette
+    minHeight: '100vh', // Container takes up full height of the viewport
   };
 
   const headingStyle = {
@@ -22,7 +23,7 @@ const Home = () => {
 
   const paragraphStyle = {
     fontSize: '1.2rem',
-    color: '#212529', // Color from my palette
+    color: '#212529', // Colour from my palette
   };
 
   return (
@@ -30,7 +31,7 @@ const Home = () => {
       <h1 style={headingStyle}>Arie David</h1>
       <img src="" alt="Arie David" style={imgStyle} />
       <p style={paragraphStyle}>Your brand statement or introduction</p>
-      <p style={paragraphStyle}>This is my portfolio site</p>
+      <p style={paragraphStyle}>Welcome to my portfolio site</p>
     </div>
   );
 }
