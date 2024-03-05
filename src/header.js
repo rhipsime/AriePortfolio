ece290d6f813f12f5bc214040677428442dc6d50
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-05 19:18:23 +0000

    Trying to style header

--- src/header.js
+++ src/header.js
@@ -1,17 +1,17 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'; 
 
 const Header = () => {
     return (
         <header style={{ backgroundColor: '#ffafcc', color: '#000' }}>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light custom-navbar">
                 <div className="container">
                     <Link className="navbar-brand" to="/">Arie David Portfolio</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav m1-auto">
+                        <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
                                 <Link className="nav-link" to="/">Home</Link>
                             </li>
