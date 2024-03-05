3e3f53b98e32798f9ae5fdb6fdf0ea60be540572
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-05 19:13:14 +0000

    trying to style the header and home page

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,40 @@
+// Home.js
+
 import React from 'react';
 
 const Home = () => {
-    return (
-      <div className="home-container">
-        <h1>Your Name</h1>
-        <img src="/Users/arie/arie-portfolio/Assets/Profile-picture.png" alt="Your Name" />
-        <p>Your brand statement or introduction</p>
-        <p>This is my portfolio site</p>
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+  const containerStyle = {
+    textAlign: 'center',
+    padding: '20px',
+    backgroundColor: '#ffc8dd', // Background color from your palette
+  };
+
+  const headingStyle = {
+    fontSize: '2rem',
+    color: '#cdb4db', // Primary color from your palette
+  };
+
+  const imgStyle = {
+    width: '200px',
+    borderRadius: '50%',
+    margin: '20px auto',
+  };
+
+  const paragraphStyle = {
+    fontSize: '1.2rem',
+    color: '#bde0fe', // Secondary color from your palette
+  };
+
+  return (
+    <div style={containerStyle}>
+      <h1 style={headingStyle}>Your Name</h1>
+      <img src="" alt="Arie David" style={imgStyle} />
+      <p style={paragraphStyle}>Your brand statement or introduction</p>
+      <p style={paragraphStyle}>This is my portfolio site</p>
+    </div>
+  );
+}
+
+export default Home;
+
+
--- src/header.js
+++ src/header.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header>
+        <header style={{ backgroundColor: '#ffafcc', color: '#000' }}>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">Your Logo</Link>
+                    <Link className="navbar-brand" to="/">Arie David Portfolio</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto">
+                        <ul className="navbar-nav m1-auto">
                             <li className="nav-item">
                                 <Link className="nav-link" to="/">Home</Link>
                             </li>
