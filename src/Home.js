598249e1fe8e429aaaa8a045efca1cc2501d91a5
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-05 18:49:26 +0000

    Added Profile Picture

--- src/Home.js
+++ src/Home.js
@@ -1,1 +1,15 @@
-export default function (){}
+import React from 'react';
+
+const Home = () => {
+    return (
+      <div className="home-container">
+        <h1>Your Name</h1>
+        <img src="/Users/arie/arie-portfolio/Assets/Profile-picture.png" alt="Your Name" />
+        <p>Your brand statement or introduction</p>
+        <p>This is my portfolio site</p>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
