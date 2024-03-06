66a427d0a37642cd0d0e171673935942414d2eba
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-06 20:15:46 +0000

    added all the export functions for Projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,1 +1,15 @@
-export default function (){}
+import React from 'react';
+import Project from './Project'; // Import the Project component
+import projectsData from './projects.json'; // Import the project data
+
+const Projects = () => {
+  return (
+    <div className="projects-container">
+      {projectsData.map((project, index) => (
+        <Project key={index} project={project} />
+      ))}
+    </div>
+  );
+}
+
+export default Projects;
