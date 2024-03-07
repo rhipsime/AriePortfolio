2ee9d23947fdd3435f3df7e7bc37e9edd16b1e39
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-07 21:49:48 +0000

    debugged the image file problem with the TA

--- src/Project.js
+++ src/Project.js
@@ -4,7 +4,7 @@ const Project = ({ project }) => {
   return (
     <div className="project">
       <h2>{project.title}</h2>
-      <img src={require(`./Assets/${project.image}`).default} alt={project.title} />
+      <img src={require(`${project.image}`)} alt={project.title} />
       <p><a href={project.deployedLink}>Deployed Version</a></p>
       <p><a href={project.githubLink}>GitHub Repository</a></p>
     </div>
