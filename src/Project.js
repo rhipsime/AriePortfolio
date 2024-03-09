506afd39d27497235eda55579dd516be563784d5
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-09 14:51:07 +0000

    updated thproject page to have rows of four

--- src/Project.js
+++ src/Project.js
@@ -4,7 +4,7 @@ const Project = ({ project }) => {
   return (
     <div className="project">
       <h2>{project.title}</h2>
-      <img src={require(`${project.image}`)} alt={project.title} className="img-fluid" />
+      <img src={require(`${project.image}`)} alt={project.title} style={{ maxWidth: '100%', height: 'auto' }} />
       <p><a href={project.deployedLink}>Deployed Version</a></p>
       <p><a href={project.githubLink}>GitHub Repository</a></p>
     </div>
--- src/Projects.js
+++ src/Projects.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Project from './Project'; // Import the Project component
 import projectsData from './projects.json'; // Import the project data
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 const Projects = () => {
   return (
-    <div className="projects-container">
-      {projectsData.map((project, index) => (
-        <Project key={index} project={project} />
-      ))}
-    </div>
+    <Container>
+      <Row xs={1} md={2} lg={4} className="g-4">
+        {projectsData.map((project, index) => (
+          <Col key={index}>
+            <Project project={project} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
