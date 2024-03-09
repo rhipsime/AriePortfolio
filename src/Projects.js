c755cda6942ac8727f6634dc00a16b3cbbff1506
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-09 15:39:14 +0000

    more css styling

--- src/Projects.js
+++ src/Projects.js
@@ -4,11 +4,10 @@ import projectsData from './projects.json'; // Import the project data
 import { Container, Row, Col } from 'react-bootstrap';
 import './Projects.css'; // Import the CSS file
 
-
 const Projects = () => {
   return (
     <Container className="projects-container"> 
-      <Row xs={1} md={2} lg={4} className="g-4">
+      <Row xs={1} md={2} className="g-4"> {/* Change lg={4} to md={2} */}
         {projectsData.map((project, index) => (
           <Col key={index}>
             <Project project={project} />
