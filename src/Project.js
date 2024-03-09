5dcb52157792ad2c950783cc8dd1c6461b334ae8
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-03-09 13:33:21 +0000

    added projects into rows of four using React Bootstrap's Container,

--- src/Project.js
+++ src/Project.js
@@ -4,7 +4,7 @@ const Project = ({ project }) => {
   return (
     <div className="project">
       <h2>{project.title}</h2>
-      <img src={require(`${project.image}`)} alt={project.title} />
+      <img src={require(`${project.image}`)} alt={project.title} className="img-fluid" />
       <p><a href={project.deployedLink}>Deployed Version</a></p>
       <p><a href={project.githubLink}>GitHub Repository</a></p>
     </div>
@@ -12,3 +12,4 @@ const Project = ({ project }) => {
 }
 
 export default Project;
+
